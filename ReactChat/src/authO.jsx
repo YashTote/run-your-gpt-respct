6c77522e51a3854f9deccf6b5ca4ede0b34ca890
YashTote <yashtote9999@gmail.com>
2023-08-24 18:43:02 +0530

    Login Changes

--- ReactChat/src/authO.jsx
+++ ReactChat/src/authO.jsx
@@ -1,17 +1,19 @@
 import { useAuth0 } from "@auth0/auth0-react";
 import './css/authO.css'
 
+//AuthO api for authentication.
 const Profile = () => {
-  const { isAuthenticated, isLoading,loginWithRedirect,logout } = useAuth0();
+  const { user, isAuthenticated, isLoading,loginWithRedirect,logout } = useAuth0();
   if (isLoading) {
     return <div>Loading ...</div>;
   }
- 
+  var userName = 'Log Out';
+  if(isAuthenticated){userName = `Log Out > ${user.name}`}
   return (
   <>
-    {(isAuthenticated) ? (<button className="btn-primary" onClick={() => logout({ logoutParams: { returnTo: window.location.origin } })}>
-      Log Out
-    </button>) : (<button className="btn-primary" onClick={() => loginWithRedirect()}>Log In</button>)}
+    {(isAuthenticated) ? (<><button className="btn-primary" onClick={() => logout({ logoutParams: { returnTo: window.location.origin } })}>
+     {userName}
+    </button></>) : (<button className="btn-primary" onClick={() => loginWithRedirect()}>Log In</button>)}
   </>
     
   );
